5726129b03c4b8116f081559503319ab85478da4
Aleks <aleks@axdrv.ru>
2022-12-07 17:12:21 +0300

    base 0.0.9

--- docs/demo/animated/assets/layout/layout.js
+++
@@ -1,6 +0,0 @@
-//components file defines components that around whole document
-import { HeaderItem } from '/assets/layout/header_item.js';
-customElements.define('header-item', HeaderItem);
-import { FooterItem } from '/assets/layout/footer_item.js';
-customElements.define('footer-item', FooterItem);
-export {HeaderItem, FooterItem}
