0aa23cb7525b76f9e9b4e2ad1a3335f26b71e9c3
Aleks <aleks@axdrv.ru>
2022-12-28 00:14:18 +0300

    C HoBblM rOgOM!!!
    minors

--- assets/sections/intro/intro_section.js
+++ assets/sections/intro/intro_section.js
@@ -1,7 +1,7 @@
 export class IntroSection extends HTMLElement {
     constructor () {
         super();
-        this.intro = "С НОВЫМ ГОДОМ!";        
+        this.intro = "Light Landing Template";        
     }
     connectedCallback() {  
         this.render();
@@ -11,11 +11,13 @@ export class IntroSection extends HTMLElement {
     } 
     attributeChangedCallback(attr, prev, next) {
         if(prev !== next) {
-          this[`${attr}`] = next;
-          this.render();
+          this.update();
        }
     }
     render() {
         this.innerHTML =  `<style>intro-section h1 {font-size: 4rem;background-image: linear-gradient(135deg, #3046AB, #9AE4C9, #24428A, #3046AB, #9AE4C9, #24428A, #3046AB, #9AE4C9, #24428A);color: transparent;-webkit-background-clip: text;background-clip: text;}</style><h1 style="text-align: center;">${this.intro}</h1>`;               
+    }
+    update() {
+
     }
 }
